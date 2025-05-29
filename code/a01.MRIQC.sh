9e46c76b1df49fea76a86f63b569836b95822922
JuliaLinke <32391611+JuliaLinke@users.noreply.github.com>
2025-05-29 17:27:49 +0200

    Update a01.MRIQC.sh

--- code/a01.MRIQC.sh
+++ code/a01.MRIQC.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+# Load MRIQC (adjust depending on your system/environment)
 module load mriqc		# depends on system settings
 
+# ==== CONFIGURATION ====
 Cohort='Cohort1'              	# adjust as needed
 Timepoint='T1'          	# adjust as needed 
-
 ROOTDIR=/MyWorkingDirectory
+
+# Swarm file to store job commands
 SWARMFILE=${ROOTDIR}/slurm/swarm_mriqc.txt
 echo -n "" > ${SWARMFILE}
 
+# ==== LOOP OVER PARTICIPANTS ====
 for s in $(cat ${ROOTDIR}/lists/${Cohort}.${Timepoint}.txt); do
-   if [[ ! -d ${ROOTDIR}/derivatives/MRIQC/${s} ]]; then
+   if [[ ! -d ${ROOTDIR}/derivatives//${Cohort}/MRIQC/${s} ]]; then
       echo "let \"rnd = \${RANDOM} % 300\"; sleep \${rnd}; \
             export TMPDIR=/lscratch/\${SLURM_JOBID}; \
             mkdir -p \${TMPDIR}/${s}.out \${TMPDIR}/${s}.wrk; \
@@ -21,4 +25,5 @@ for s in $(cat ${ROOTDIR}/lists/${Cohort}.${Timepoint}.txt); do
    fi
 done
 
+# ==== SUBMIT SWARM JOB ====
 swarm -f ${SWARMFILE} -g 40 -t auto --gres=lscratch:40 --logdir ${ROOTDIR}/slurm --time=120:00:00 --job-name Efficiency
